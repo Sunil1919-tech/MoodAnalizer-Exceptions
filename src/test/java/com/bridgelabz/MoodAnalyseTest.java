77c3faeda6d32cdff57ea5894fbe5b5cea274120
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-08-31 15:34:02 +0530

    Added :Custom Exception Handling

--- src/test/java/com/bridgelabz/MoodAnalyseTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyseTest.java
@@ -16,10 +16,13 @@ public class MoodAnalyseTest {
     @Test
     public void givenMessage_SadMood_Should_Return_Sad() {
         MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain("I am In a Sad Mood");
-        String mood = moodAnalyzer.analyseMood();
-        //Asserting The Result As Boolean Type True Or False
-        Assertions.assertEquals("SAD", mood);
-
+        String mood;
+        try {
+            mood = moodAnalyzer.analyseMood();
+            Assertions.assertEquals("SAD", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
 
     }
 
@@ -27,14 +30,12 @@ public class MoodAnalyseTest {
     @Test
     public void givenMessage_AnyMood_Should_Return_HAPPY() {
         MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain("I am in Happy Mood");
-        String mood = moodAnalyser.analyseMood();
-        Assertions.assertEquals("HAPPY", mood);
-    }
-
-    @Test
-    public void given_NULLMood_Should_Return_HAPPY() {
-        MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain(null);
-        String mood = moodAnalyser.analyseMood();
-        Assertions.assertEquals("HAPPY", mood);
+        String mood;
+        try {
+            mood = moodAnalyser.analyseMood();
+            Assertions.assertEquals("HAPPY", mood);
+        } catch (MoodAnalysisException e) {
+            e.printStackTrace();
+        }
     }
 }
