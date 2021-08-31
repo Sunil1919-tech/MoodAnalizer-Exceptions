81a926fdb222445a7ea1144e4bf7650591d2093b
Sunil Gollapalli <sunigollapalli19@gmail.com>
2021-08-31 13:57:24 +0530

    Added: Mood Should return happy

--- src/test/java/com/bridgelabz/MoodAnalyseTest.java
+++ src/test/java/com/bridgelabz/MoodAnalyseTest.java
@@ -10,12 +10,19 @@ import org.junit.jupiter.api.Test;
  * @since 28/08/2021
  */
 public class MoodAnalyseTest {
+    //testCase to predict the mood happy/Sad
     @Test
     public void given_SadMood_Should_Return_Sad() {
         MoodAnalyzerMain moodAnalyzer = new MoodAnalyzerMain();
         String mood = moodAnalyzer.analyseMood("I am In a Sad Mood");
         Assertions.assertEquals("SAD", mood);
 
+    }
+    @Test
+    public void given_AnyMood_Should_Return_HAPPY() {
+        MoodAnalyzerMain moodAnalyser = new MoodAnalyzerMain();
+        String mood = moodAnalyser.analyseMood("I am in Any Mood");
+        Assertions.assertEquals("HAPPY", mood);
     }
 
 }
